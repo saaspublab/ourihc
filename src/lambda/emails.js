52b32dd0ff3571dd39263502efd6d1180a9d753f
Jay S <allesyaj@gmail.com>
2020-09-14 18:03:48 -0400

    feat: revamp /join page for house meeting; pull from db

---
+++ src/lambda/emails.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+const responseHeaders = {
+  'Access-Control-Allow-Origin': process.env.CORS_URL,
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, OPTIONS',
+};
+const fetchHeaders = {
+  'cache-control': 'no-cache',
+  'content-type': 'application/json',
+  'x-apikey': process.env.X_API_KEY,
+};
+
+/**
+ * Fetch all students from database.
+ *
+ * @method fetchEmails
+ * @param query {string}
+ * @param method {string}
+ *
+ * @return {object}
+ */
+async function fetchEmails(query, method) {
+  const response = await fetch(`${process.env.API_URL}/students${query}`, {
+    method,
+    headers: fetchHeaders,
+  });
+
+  return response;
+}
+
+exports.handler = async (event) => {
+  const path = event.path.replace(/api\/[^/]+/, '');
+  const segments = path.split('/').filter((e) => e);
+
+  switch (event.httpMethod) {
+    case 'GET':
+      /* GET /api/emails */
+      if (segments.length === 0) {
+        try {
+          const response = await fetchEmails('', 'get').then((res) =>
+            res.json()
+          );
+
+          return {
+            statusCode: 200,
+            headers: responseHeaders,
+            body: JSON.stringify({
+              success: true,
+              meta: {
+                totalStudents: response.length,
+              },
+              data: response,
+            }),
+          };
+        } catch (err) {
+          return {
+            statusCode: err.statusCode || 500,
+            headers: responseHeaders,
+            body: JSON.stringify({
+              success: false,
+              error: err.message,
+            }),
+          };
+        }
+      }
+
+      return {
+        statusCode: 400,
+        headers: responseHeaders,
+        body: JSON.stringify({
+          success: false,
+          error: 'Bad request: too many segments in GET request',
+        }),
+      };
+
+    case 'OPTIONS':
+      return {
+        statusCode: 204,
+        headers: responseHeaders,
+      };
+
+    /* Fallthrough case */
+    default:
+      return {
+        statusCode: 405,
+        headers: responseHeaders,
+        body: JSON.stringify({
+          success: false,
+          error: 'HTTP method not allowed; must be one of GET/OPTIONS',
+        }),
+      };
+  }
+};
--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,14 +1,225 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useStickyState from '../components/UseStickyState';
 import styles from './nope.module.sass';
 
+const links = [
+  'https://saintanselms.zoom.us/j/98405501386?pwd=RHhKTkpTdFY5SEJoOWF1d0gyYk1kUT09',
+  'https://saintanselms.zoom.us/j/97672684220?pwd=VmEvTmJ1ODU0anVCRWgzQUVRWFladz09',
+  'https://saintanselms.zoom.us/j/92911504282?pwd=WlMrb1hXMnFPQVFjV3hmUU4wdEZZQT09',
+  'https://saintanselms.zoom.us/j/95266370870?pwd=Tm9MdDhZUW1jRWZrMU9haDBWTlpSdz09',
+];
+
+const currentHour = new Date().getHours();
+let greetingMessage;
+
+if (currentHour >= 4 && currentHour < 12) {
+  // after 4:00AM and before 12:00PM
+  greetingMessage = 'Good morning';
+} else if (currentHour >= 12 && currentHour <= 17) {
+  // after 12:00PM and before 6:00pm
+  greetingMessage = 'Good afternoon';
+} else if (currentHour > 17 || currentHour < 4) {
+  // after 5:59pm or before 4:00AM (to accommodate night owls)
+  greetingMessage = 'Good evening';
+} else {
+  // if for some reason the calculation didn't work
+  greetingMessage = 'Welcome';
+}
+
+export const useInput = (initialValue) => {
+  const [value, setValue] = useStickyState(initialValue, 'email');
+
+  return {
+    value,
+    setValue,
+    bind: {
+      value,
+      onChange: (event) => {
+        setValue(event.target.value);
+      },
+    },
+  };
+};
+
+function getLinkByHouse(house) {
+  let linkIndex;
+
+  switch (house) {
+    case 'Alban':
+      linkIndex = 0;
+      break;
+
+    case 'Austin':
+      linkIndex = 1;
+      break;
+
+    case 'Main':
+      linkIndex = 2;
+      break;
+
+    case 'Moore':
+      linkIndex = 3;
+      break;
+
+    default:
+      break;
+  }
+
+  return links[linkIndex];
+}
+
 function Join() {
+  const { value: email, bind: bindEmail } = useInput('@saintanselms.org');
+  const [students, setStudents] = useState({});
+
+  async function fetchStudents() {
+    await fetch('/api/emails/')
+      .then((res) => res.json())
+      .then((res) => {
+        setStudents(res.data);
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  let house;
+  let nickname;
+
+  if (
+    students.length > 1 &&
+    email.length > 1 &&
+    students.filter((e) => e.emailAddress === email).length > 0
+  ) {
+    house = students.find((obj) => obj.emailAddress === email).house;
+    nickname = students.find((obj) => obj.emailAddress === email).nickname;
+  }
+
   return (
     <div className={[styles.wrapper, 'page--maroon'].join(' ')}>
-      <h1>House Day 2020</h1>
+      <h4>Tuesday, September 15, 2020</h4>
+      <h1>House Meetings</h1>
       <p>
-        Thank you to everyone who participated in House Day 2020 and
-        congratulations to Austin House for their win.
+        <strong>
+          At 3:00 PM this Tuesday (9/15), each student will be required to
+          attend a 30-minute House Meeting.
+        </strong>{' '}
+        During these meetings, each House's members meet, stratagize, and
+        discuss events in their race to the House Cup.
       </p>
+
+      {!students ||
+        (!students.length && (
+          <section className="content--block bordered">
+            <h3 className="heading">Loading... Please wait...</h3>
+            <p>
+              If this message remains for more than 5-10 seconds, please message
+              us as soon as possible on <Link to="/discord">Discord</Link>,{' '}
+              <a
+                href="//instagram.com/inter.house.council"
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+              >
+                Instagram
+              </a>
+              , or by{' '}
+              <a
+                href="mailto:ihc@saintanselms.org"
+                target="_blank"
+                rel="noopener noreferrer nofoollow"
+              >
+                email (ihc@saintanselms.org)
+              </a>
+              .
+            </p>
+          </section>
+        ))}
+
+      {students.length > 1 &&
+        (!email ||
+          !students.filter((e) => e.emailAddress === email).length > 0) && (
+          <section className="content--block bordered">
+            <h3 className="heading">Your School Email Address + Zoom Link</h3>
+
+            <p>
+              <b>IMPORTANT:</b> once you type your email address, you will not
+              be able to change it.
+            </p>
+
+            <br />
+
+            <form style={{ margin: 0 }}>
+              <label htmlFor="email">
+                School Email Address
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="lserif@saintanselms.org"
+                  required
+                  {...bindEmail}
+                />
+              </label>
+            </form>
+
+            {email.length >= '@saintanselms.org'.length + 3 && (
+              <p style={{ marginTop: 0 }}>
+                <b>
+                  Hmmm... We weren't able to find a student with that email
+                  address.
+                </b>{' '}
+                Check your spelling, and if the error persists, please message
+                us as soon as possible on <Link to="/discord">Discord</Link>,{' '}
+                <a
+                  href="//instagram.com/inter.house.council"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  Instagram
+                </a>
+                , or by{' '}
+                <a
+                  href="mailto:ihc@saintanselms.org"
+                  target="_blank"
+                  rel="noopener noreferrer nofoollow"
+                >
+                  email (ihc@saintanselms.org)
+                </a>
+                .
+              </p>
+            )}
+          </section>
+        )}
+
+      {students.length > 1 &&
+        email.length > 1 &&
+        students.filter((e) => e.emailAddress === email).length > 0 && (
+          <>
+            <section className="content--block bordered">
+              <h3 className="heading">{`${greetingMessage}, ${nickname}!`}</h3>
+
+              <p>
+                To access <b>{house} House</b>'s meeting room, click the link
+                below.
+              </p>
+
+              <p>
+                <a
+                  href={getLinkByHouse(house)}
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  {getLinkByHouse(house)}
+                </a>
+              </p>
+            </section>
+          </>
+        )}
     </div>
   );
 }
